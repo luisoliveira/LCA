f6013275234efe6e196b02f13af2ece5aadc92e3
Luis Oliveira <luishenrique.ao@gmail.com>
2012-09-13 01:51:33 -0300

    ProcessoDAOImpl

---
+++ ProjetoLCA/src/com/voidsoftware/lca/persistence/dao/ProcessoDaoImpl.java
@@ -0,0 +1,33 @@
+package com.voidsoftware.lca.persistence.dao;
+
+import com.voidsoftware.lca.beans.Processo;
+import com.voidsoftware.lca.persistence.interfaces.ProcessoDAO;
+
+public class ProcessoDaoImpl implements ProcessoDAO {
+
+	@Override
+	public void cadastrar(Processo processo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Processo processo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remover(int numeroProcesso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Processo consultar(int numeroProcesso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
